dbbe10a40fafe14e43aefb5b71c4862d51a40b16
Matthew Smith <musicsmith4@gmail.com>
2021-04-27 15:55:08 -0400

    update desktop bg image styles across site

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-import { DESKTOP_BP, TABLET_BP } from '../../constants/constants';
+import { TABLET_BP } from '../../constants/constants';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 
 const StyledMain = styled.main`
   @media screen and (min-width: ${TABLET_BP}em) {
     padding: 0 var(--site-side-padding-md);
+  }
+`;
+
+const StyledBgImages = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+
+  .leaf-bg-top {
+    display: ${(props) => (props.hideTopImage ? 'none' : 'block')};
+    top: ${(props) => (props.useAlternative ? '59%' : '35%')};
+  }
+
+  .leaf-bg-bottom {
+    display: ${(props) => (props.useAlternative ? 'block' : 'none')};
+    top: 200%;
+    right: 0;
+    transform: rotate(180deg);
   }
 `;
 
@@ -19,6 +42,14 @@ const Layout = ({
   children: React.ReactNode;
   pageTitle?: string;
 }) => {
+  const router = useRouter();
+  const useAlternativeBgImagesStyles =
+    router.asPath === '/' ||
+    router.asPath === '/about' ||
+    router.asPath === '/contact';
+  const hideTopBgImage =
+    router.asPath === '/locations' || router.asPath === '/contact';
+
   return (
     <>
       <Head>
@@ -27,6 +58,28 @@ const Layout = ({
         </title>
       </Head>
       <Header></Header>
+      <StyledBgImages
+        className='hidden-sm hidden-md'
+        useAlternative={useAlternativeBgImagesStyles}
+        hideTopImage={hideTopBgImage}
+      >
+        <div className='bg-image leaf-bg-top'>
+          <Image
+            src='/shared/desktop/bg-pattern-leaf.svg'
+            layout='fixed'
+            width={1006}
+            height={594}
+          />
+        </div>
+        <div className='bg-image leaf-bg-bottom'>
+          <Image
+            src='/shared/desktop/bg-pattern-leaf.svg'
+            layout='fixed'
+            width={1006}
+            height={594}
+          />
+        </div>
+      </StyledBgImages>
       <StyledMain>{children}</StyledMain>
       <Footer />
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,12 @@
-import styled from 'styled-components';
-import Image from 'next/image';
-
 import Layout from '../components/layout/layout';
 import ContentContainer from '../components/layout/content-container';
 import PageIntroCard from '../components/pages/home/page-intro-card';
 import ProductLinksList from '../components/products/product-links-list';
 import QualitiesList from '../components/pages/home/qualities-list';
 
-const StyledBgImages = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-
-  .leaf-bg-top {
-    top: 47%;
-  }
-
-  .leaf-bg-bottom {
-    top: 138%;
-    right: 0;
-    transform: rotate(180deg);
-  }
-`;
-
 export default function Home() {
   return (
     <Layout>
-      <StyledBgImages className='hidden-sm hidden-md'>
-        <div className='bg-image leaf-bg-top'>
-          <Image
-            src='/shared/desktop/bg-pattern-leaf.svg'
-            layout='fixed'
-            width={1006}
-            height={594}
-          />
-        </div>
-        <div className='bg-image leaf-bg-bottom'>
-          <Image
-            src='/shared/desktop/bg-pattern-leaf.svg'
-            layout='fixed'
-            width={1006}
-            height={594}
-          />
-        </div>
-      </StyledBgImages>
       <PageIntroCard />
       <ContentContainer isHome={true}>
         <ProductLinksList />
