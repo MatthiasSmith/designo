4a2c62a74af9a6e29530dede1eb4400a9db5e5b1
Matthew Smith <musicsmith4@gmail.com>
2021-05-03 14:56:02 -0400

    add scroll animations to project list items

--- components/projects/project-list-item.tsx
+++ components/projects/project-list-item.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Scene } from 'react-scrollmagic';
+
 import { DESKTOP_BP } from '../../constants/constants';
 import ProjectCard from './project-card';
 
 const StyledProjectListItem = styled.li`
   cursor: pointer;
+
+  &.animatable {
+    transition: all 0.8s ease-out;
+    opacity: 0;
+
+    &:nth-of-type(even) {
+      transform: translateX(50%);
+    }
+
+    &:nth-of-type(odd) {
+      transform: translateX(-50%);
+    }
+
+    @media screen and (prefers-reduced-motion: reduce) {
+      transition: opacity 0.6s ease-out;
+      transform: unset;
+    }
+  }
+
+  &.fade-in.animatable {
+    opacity: 1;
+    transform: translate(0rem, 0rem);
+  }
 
   &:not(:first-of-type) {
     margin-top: 2.5rem;
@@ -25,15 +50,17 @@ const StyledProjectListItem = styled.li`
 
 const ProjectListItem = ({ project }) => {
   return (
-    <StyledProjectListItem className='flex-col'>
-      <a className='link-card'>
-        <ProjectCard
-          image={project.image}
-          title={project.title}
-          description={project.description}
-        />
-      </a>
-    </StyledProjectListItem>
+    <Scene classToggle='fade-in' reverse={false}>
+      <StyledProjectListItem className='flex-col animatable'>
+        <a className='link-card'>
+          <ProjectCard
+            image={project.image}
+            title={project.title}
+            description={project.description}
+          />
+        </a>
+      </StyledProjectListItem>
+    </Scene>
   );
 };
 
