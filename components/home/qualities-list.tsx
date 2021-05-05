61a4e30857a13ee4d90c244f9e61ed4210d00643
Matthew Smith <musicsmith4@gmail.com>
2021-05-05 16:09:29 -0400

    update padding on quality-list for desktop devices

--- components/home/qualities-list.tsx
+++ components/home/qualities-list.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import { DESKTOP_BP } from '../../constants/constants';
 
 import qualities from '../../data/qualities';
 import QualityItem from './quality-item';
 
 const Styled = styled.ul`
   padding: 7.5rem 0 0;
+
+  @media screen and (min-width: ${DESKTOP_BP}em) {
+    padding: 10rem 0 0;
+  }
 `;
 
 const QualitiesList = () => {
